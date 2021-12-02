71df07f2204b3ec33924730ea165dfd3a0f20dc1
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-12-02 10:06:02 -0300

    chore: add eslint, prettier and format current code

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,9 +1,9 @@
-import express from 'express'
-import cors from 'cors'
+import cors from "cors";
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-export { app }
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,3 @@
-import { app } from './app'
+import { app } from "./app";
 
-app.listen(3333, () => console.log('👽️ Server is running on port 3333 👽️'))
+app.listen(3333, () => console.log("👽️ Server is running on port 3333 👽️"));
