449a0ced6ab7eb3e5a6e4523566a19c32450dbc6
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-12-02 10:42:50 -0300

    chore: add AppError and global exception handler

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,11 @@
+import "dotenv/config";
 import "reflect-metadata";
+import "express-async-errors";
 
 import cors from "cors";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
+import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
 createConnection();
@@ -11,5 +14,20 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      message: `Internal Server Error + ${err.message}`,
+    });
+  }
+);
 
 export { app };
