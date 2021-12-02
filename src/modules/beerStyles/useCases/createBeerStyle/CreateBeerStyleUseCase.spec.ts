3f80b408547f2356017558d109683ff99255aaa3
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-12-02 13:01:34 -0300

    test: should not be able to create two beer styles with same name

--- src/modules/beerStyles/useCases/createBeerStyle/CreateBeerStyleUseCase.spec.ts
+++ src/modules/beerStyles/useCases/createBeerStyle/CreateBeerStyleUseCase.spec.ts
@@ -1,5 +1,7 @@
 import { FakeBeerStylesRepository } from "@modules/beerStyles/repositories/fakes/FakeBeerStylesRepository";
 
+import { AppError } from "@shared/errors/AppError";
+
 import { CreateBeerStyleUseCase } from "./CreateBeerStyleUseCase";
 
 let createBeerStyleUseCase: CreateBeerStyleUseCase;
@@ -13,7 +15,7 @@ describe("Create Beer Style", () => {
     );
   });
 
-  it("should be able to create a new Beer Style", async () => {
+  it("should be able to create a new beer style", async () => {
     const beerStyle = await createBeerStyleUseCase.execute({
       name: "a beer style name",
       minimum_temperature: -5,
@@ -24,5 +26,19 @@ describe("Create Beer Style", () => {
     expect(beerStyle.name).toEqual("a beer style name");
     expect(beerStyle.minimum_temperature).toBe(-5);
     expect(beerStyle.maximum_temperature).toBe(5);
+  });
+
+  it("should not be able to create two beer styles with the same name", async () => {
+    const beerStyleData = {
+      name: "a beer style name",
+      minimum_temperature: -5,
+      maximum_temperature: 5,
+    };
+
+    await fakeBeerStylesRepository.create(beerStyleData);
+
+    await expect(createBeerStyleUseCase.execute(beerStyleData)).rejects.toEqual(
+      new AppError("A beer style with this name already exists!")
+    );
   });
 });
