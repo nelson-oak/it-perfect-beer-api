815bc01145a342aca7e771bc791a7273eba8bab4
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-12-02 18:25:29 -0300

    feat: import beer style by csv

--- src/modules/beerStyles/useCases/importBeerStyleByCsv/ImportBeerStyleByCsvUseCase.ts
+++ src/modules/beerStyles/useCases/importBeerStyleByCsv/ImportBeerStyleByCsvUseCase.ts
@@ -1,14 +1,12 @@
 import { IBeerStyleRepository } from "@modules/beerStyles/repositories/IBeerStylesRepository";
+import { parse } from "csv-parse";
+import fs from "fs";
 import { inject, injectable } from "tsyringe";
 
 interface IBeerStyle {
   name: string;
   minimum_temperature: number;
   maximum_temperature: number;
-}
-
-interface IRequest {
-  csvData: IBeerStyle[];
 }
 
 @injectable()
@@ -18,8 +16,83 @@ class ImportBeerStyleByCsvUseCase {
     private beerStylesRepository: IBeerStyleRepository
   ) {}
 
-  async execute({ csvData }: IRequest) {
-    throw new Error("Method not implemented.");
+  private loadBeerStyles(file: Express.Multer.File): Promise<IBeerStyle[]> {
+    return new Promise((resolve, reject) => {
+      const beerStyles: IBeerStyle[] = [];
+
+      const stream = fs.createReadStream(file.path);
+
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, minimum_temperature, maximum_temperature] = line;
+
+          beerStyles.push({
+            name,
+            minimum_temperature,
+            maximum_temperature,
+          });
+        })
+        .on("end", () => {
+          fs.promises.unlink(file.path);
+          resolve(beerStyles);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File) {
+    const beerStyles = await this.loadBeerStyles(file);
+
+    const duplicate: string[] = [];
+    const created: string[] = [];
+    const error: string[] = [];
+
+    beerStyles.map(
+      async ({ name, minimum_temperature, maximum_temperature }) => {
+        if (
+          !name ||
+          name.length === 0 ||
+          (!minimum_temperature && minimum_temperature !== 0) ||
+          (!maximum_temperature && maximum_temperature !== 0)
+        ) {
+          if (!duplicate.includes(name)) {
+            error.push(name);
+          }
+        }
+
+        if (minimum_temperature > maximum_temperature) {
+          error.push(name);
+        }
+
+        const beerStyleExists = await this.beerStylesRepository.findByName(
+          name
+        );
+
+        if (!beerStyleExists) {
+          await this.beerStylesRepository.create({
+            name,
+            minimum_temperature,
+            maximum_temperature,
+          });
+
+          created.push(name);
+        } else if (!duplicate.includes(name)) {
+          duplicate.push(name);
+        }
+      }
+    );
+
+    return {
+      created,
+      duplicate,
+      error,
+    };
   }
 }
 
